bd4cb21edf5b84ce297d9253fade74fe116f60dc
Raman Rasliuk <roma@redbox.by>
2023-06-09 10:13:49 +0200

    Dart 3 and nullsafety support

--- lib/waveprogressbar_flutter.dart
+++ lib/waveprogressbar_flutter.dart
@@ -2,15 +2,24 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'dart:ui';
 
-class WaveProgressBar extends StatefulWidget{
+class WaveProgressBar extends StatefulWidget {
+  WaveProgressBar({
+    required this.size,
+    this.waveHeight = 25.0,
+    required this.percentage,
+    required this.textStyle,
+    this.waveDistance = 30.0,
+    this.flowSpeed = 1.5,
+    this.waterColor = const Color(0xffe16009),
+    this.strokeCircleColor = const Color(0xFFE0E0E0),
+    this.circleStrokeWidth = 4.0,
+    required this.heightController,
+  });
 
   final Size size;
-  ///决定 水平面的高度
   final double percentage;
-  ///浪尖峰
   final double waveHeight;
   final TextStyle textStyle;
-  ///波距  其实是1/4波距
   final double waveDistance;
   final double flowSpeed;
   final Color waterColor;
@@ -18,38 +27,24 @@ class WaveProgressBar extends StatefulWidget{
   final double circleStrokeWidth;
   final WaterController heightController;
 
-
-  WaveProgressBar({
-    @required this.size,
-    this.waveHeight : 25.0,
-    @required this.percentage,
-    @required this.textStyle,
-    this.waveDistance : 30.0,
-    this.flowSpeed : 1.5,
-    this.waterColor : const Color(0xffe16009),
-    this.strokeCircleColor : const Color(0xFFE0E0E0),
-    this.circleStrokeWidth : 4.0,
-    @required this.heightController,
-  });
-
   @override
   State<StatefulWidget> createState() {
     return new WaveProgressBarState();
   }
 }
 
-
-class WaveProgressBarState extends State<WaveProgressBar> with SingleTickerProviderStateMixin{
-  double _moveForwardDark=0.0;
-  double _moveForwardLight=0.0;
-  double _waterHeight;
-  double _percentage;
-  Animation<double> animation;
-  AnimationController animationController;
-  VoidCallback _voidCallback;
-  Random _random=new Random();
-  Picture _lightWavePic;
-  Picture _darkWavePic;
+class WaveProgressBarState extends State<WaveProgressBar>
+    with SingleTickerProviderStateMixin {
+  double _moveForwardDark = 0.0;
+  double _moveForwardLight = 0.0;
+  late double _waterHeight;
+  late double _percentage;
+  late Animation<double> animation;
+  late AnimationController animationController;
+  late VoidCallback _voidCallback;
+  Random _random = new Random();
+  late Picture _lightWavePic;
+  late Picture _darkWavePic;
 
   @override
   void dispose() {
@@ -61,27 +56,31 @@ class WaveProgressBarState extends State<WaveProgressBar> with SingleTickerProvi
   @override
   void initState() {
     super.initState();
-    _percentage=widget.percentage;
-    _waterHeight=(1-_percentage) * widget.size.height;
-    widget.heightController.bezierCurveState=this;
-    WavePictureGenerator generator=WavePictureGenerator(widget.size,widget.waveDistance,widget.waveHeight,widget.waterColor);
-    _lightWavePic=generator.drawLightWave();
-    _darkWavePic=generator.drawDarkWave();
-    animationController = new AnimationController(duration: const Duration(milliseconds: 3000), vsync: this);
+    _percentage = widget.percentage;
+    _waterHeight = (1 - _percentage) * widget.size.height;
+    widget.heightController.bezierCurveState = this;
+    WavePictureGenerator generator = WavePictureGenerator(
+        widget.size, widget.waveDistance, widget.waveHeight, widget.waterColor);
+    _lightWavePic = generator.drawLightWave();
+    _darkWavePic = generator.drawDarkWave();
+    animationController = new AnimationController(
+        duration: const Duration(milliseconds: 3000), vsync: this);
 
-    WidgetsBinding widgetsBinding=WidgetsBinding.instance;
-    widgetsBinding.addPostFrameCallback((callback){
+    WidgetsBinding widgetsBinding = WidgetsBinding.instance;
+    widgetsBinding.addPostFrameCallback((callback) {
       widgetsBinding.addPersistentFrameCallback((callback) {
-        if(mounted){
+        if (mounted) {
           setState(() {
-            _moveForwardDark= _moveForwardDark - widget.flowSpeed- _random.nextDouble()-1;
-            if(_moveForwardDark<= - widget.waveDistance*4){
-              _moveForwardDark=_moveForwardDark+widget.waveDistance*4;
+            _moveForwardDark =
+                _moveForwardDark - widget.flowSpeed - _random.nextDouble() - 1;
+            if (_moveForwardDark <= -widget.waveDistance * 4) {
+              _moveForwardDark = _moveForwardDark + widget.waveDistance * 4;
             }
 
-            _moveForwardLight = _moveForwardLight- widget.flowSpeed- _random.nextDouble();
-            if(_moveForwardLight <= - widget.waveDistance*4){
-              _moveForwardLight = _moveForwardLight+widget.waveDistance*4;
+            _moveForwardLight =
+                _moveForwardLight - widget.flowSpeed - _random.nextDouble();
+            if (_moveForwardLight <= -widget.waveDistance * 4) {
+              _moveForwardLight = _moveForwardLight + widget.waveDistance * 4;
             }
           });
           widgetsBinding.scheduleFrame();
@@ -90,18 +89,16 @@ class WaveProgressBarState extends State<WaveProgressBar> with SingleTickerProvi
     });
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return new CustomPaint(
       size: widget.size,
       painter: new BezierCurvePainter(
-        moveForward:_moveForwardDark,
-        textStyle:widget.textStyle ,
+        moveForward: _moveForwardDark,
+        textStyle: widget.textStyle,
         circleStrokeWidth: widget.circleStrokeWidth,
         strokeCircleColor: widget.strokeCircleColor,
-        percentage: _percentage ,
+        percentage: _percentage,
         moveForwardLight: _moveForwardLight,
         lightWavePic: _lightWavePic,
         darkWavePic: _darkWavePic,
@@ -110,114 +107,113 @@ class WaveProgressBarState extends State<WaveProgressBar> with SingleTickerProvi
     );
   }
 
-  void changeWaterHeight(double h){
-    initAnimation(_percentage ,h);
+  void changeWaterHeight(double h) {
+    initAnimation(_percentage, h);
     animationController.forward();
   }
 
+  void initAnimation(double old, double newPercentage) {
+    animation =
+        new Tween(begin: old, end: newPercentage).animate(animationController);
 
-  void initAnimation(double old ,double newPercentage){
-    animation = new Tween(begin: old, end: newPercentage).animate(animationController);
-
-    animation.addListener(_voidCallback=() {
+    animation.addListener(_voidCallback = () {
       setState(() {
         double value = animation.value;
-        _percentage=value;
-        double newHeight=(1-_percentage) * widget.size.height;
-        _waterHeight=newHeight;
+        _percentage = value;
+        double newHeight = (1 - _percentage) * widget.size.height;
+        _waterHeight = newHeight;
       });
     });
 
-    animation.addStatusListener((animationStatus){
-      if(animationStatus==AnimationStatus.completed){
+    animation.addStatusListener((animationStatus) {
+      if (animationStatus == AnimationStatus.completed) {
         animation.removeListener(_voidCallback);
         animationController.reset();
-      }else if(animationStatus==AnimationStatus.forward){
-
-      }
+      } else if (animationStatus == AnimationStatus.forward) {}
     });
   }
 }
 
-
-class WavePictureGenerator{
-  PictureRecorder _recorder;
+class WavePictureGenerator {
+  late PictureRecorder _recorder;
   final Size size;
   final double _waveDistance;
   final double _waveHeight;
   final Color _waterColor;
-  int _maxCount;
-  double waterHeight;
-  double _targetWidth;
-  double _targetHeight;
+  late int _maxCount;
+  late double waterHeight;
+  late double _targetWidth;
+  late double _targetHeight;
 
-  WavePictureGenerator(this.size,this._waveDistance,this._waveHeight,this._waterColor){
-    double oneDistance=_waveDistance*4;
-    int count=(size.width/oneDistance).ceil()+1;
-    _targetWidth=count*oneDistance;
-    _maxCount=count*4+1;
-    waterHeight=size.height/2;
-    _targetHeight=size.height+waterHeight;
+  WavePictureGenerator(
+    this.size,
+    this._waveDistance,
+    this._waveHeight,
+    this._waterColor,
+  ) {
+    double oneDistance = _waveDistance * 4;
+    int count = (size.width / oneDistance).ceil() + 1;
+    _targetWidth = count * oneDistance;
+    _maxCount = count * 4 + 1;
+    waterHeight = size.height / 2;
+    _targetHeight = size.height + waterHeight;
   }
 
-  Picture drawDarkWave(){
+  Picture drawDarkWave() {
     return drawWaves(true);
   }
 
-  Picture drawLightWave(){
+  Picture drawLightWave() {
     return drawWaves(false);
   }
 
-  Picture drawWaves(bool isDarkWave){
-    _recorder=PictureRecorder();
-    Canvas canvas=Canvas(_recorder);
+  Picture drawWaves(bool isDarkWave) {
+    _recorder = PictureRecorder();
+    Canvas canvas = Canvas(_recorder);
     canvas.clipRect(new Rect.fromLTWH(0.0, 0.0, _targetWidth, _targetHeight));
-    Paint paint =Paint();
-    if(isDarkWave){
+    Paint paint = Paint();
+    if (isDarkWave) {
       paint.color = _waterColor;
-    }else{
+    } else {
       paint.color = _waterColor.withAlpha(0x88);
     }
-    paint.style=PaintingStyle.fill;
+    paint.style = PaintingStyle.fill;
     canvas.drawPath(createBezierPath(isDarkWave), paint);
     return _recorder.endRecording();
   }
 
-
-  Path createBezierPath(bool isDarkWave){
-
-    Path path=Path();
+  Path createBezierPath(bool isDarkWave) {
+    Path path = Path();
     path.moveTo(0, waterHeight);
 
-    double lastPoint=0.0;
-    int m=0;
+    double lastPoint = 0.0;
+    int m = 0;
     double waves;
-    for(int i=0;i<_maxCount-2;i=i+2){
-      if(m%2==0){
-        waves=waterHeight+_waveHeight;
-      }else{
-        waves=waterHeight-_waveHeight;
+    for (int i = 0; i < _maxCount - 2; i = i + 2) {
+      if (m % 2 == 0) {
+        waves = waterHeight + _waveHeight;
+      } else {
+        waves = waterHeight - _waveHeight;
       }
-      path.cubicTo(lastPoint, waterHeight,   lastPoint+_waveDistance, waves,   lastPoint+_waveDistance*2, waterHeight);
-      lastPoint=lastPoint+_waveDistance*2;
+      path.cubicTo(lastPoint, waterHeight, lastPoint + _waveDistance, waves,
+          lastPoint + _waveDistance * 2, waterHeight);
+      lastPoint = lastPoint + _waveDistance * 2;
       m++;
     }
-    if(isDarkWave){
-      path.lineTo(lastPoint,_targetHeight);
-      path.lineTo(0,_targetHeight);
-    }else{
-      double waveHeightMax=waterHeight+_waveHeight+10.0;
+    if (isDarkWave) {
+      path.lineTo(lastPoint, _targetHeight);
+      path.lineTo(0, _targetHeight);
+    } else {
+      double waveHeightMax = waterHeight + _waveHeight + 10.0;
       path.lineTo(lastPoint, waveHeightMax);
-      path.lineTo(0,waveHeightMax);
+      path.lineTo(0, waveHeightMax);
     }
     path.close();
     return path;
   }
 }
 
-
-class BezierCurvePainter extends CustomPainter{
-
+class BezierCurvePainter extends CustomPainter {
   final double moveForward;
   final Color strokeCircleColor;
   final TextStyle textStyle;
@@ -227,63 +223,68 @@ class BezierCurvePainter extends CustomPainter{
   final Picture darkWavePic;
   final Picture lightWavePic;
   final double waterHeight;
-  final Paint _paints =Paint();
+  final Paint _paints = Paint();
 
   BezierCurvePainter({
-    @required this.moveForward,
-    @required this.strokeCircleColor,
-    @required this.textStyle,
-    @required this.circleStrokeWidth,
-    @required this.percentage,
-    @required this.moveForwardLight,
-    @required this.darkWavePic,
-    @required this.lightWavePic,
-    @required this.waterHeight,
+    required this.moveForward,
+    required this.strokeCircleColor,
+    required this.textStyle,
+    required this.circleStrokeWidth,
+    required this.percentage,
+    required this.moveForwardLight,
+    required this.darkWavePic,
+    required this.lightWavePic,
+    required this.waterHeight,
   });
 
   @override
   void paint(Canvas canvas, Size size) {
-    Paint layerPaint=new Paint();
+    Paint layerPaint = new Paint();
 
-    double halfSizeHeight = size.height/2;
-    double halfSizeWidth = size.width/2;
-    double radius=min(size.width, size.height)/2-circleStrokeWidth/2;
+    double halfSizeHeight = size.height / 2;
+    double halfSizeWidth = size.width / 2;
+    double radius = min(size.width, size.height) / 2 - circleStrokeWidth / 2;
 
     //由于在绘制图片的时候在波浪上面有50%高度的空白部分，所以在这里必须减掉
-    double targetHeight=waterHeight-halfSizeHeight;
+    double targetHeight = waterHeight - halfSizeHeight;
 
-    canvas.saveLayer(new Rect.fromLTRB(0.0, 0.0, size.width, size.height), layerPaint);
+    canvas.saveLayer(
+        new Rect.fromLTRB(0.0, 0.0, size.width, size.height), layerPaint);
     //绘制淡颜色的海浪
     canvas.save();
     canvas.translate(moveForwardLight, targetHeight);
     canvas.drawPicture(lightWavePic);
 
     //绘制深颜色的海浪
-    double moveDistance=moveForward-moveForwardLight;
+    double moveDistance = moveForward - moveForwardLight;
     canvas.translate(moveDistance, 0.0);
     canvas.drawPicture(darkWavePic);
     canvas.restore();
 
-    layerPaint.blendMode=BlendMode.dstIn ;
-    canvas.saveLayer(new Rect.fromLTRB(0.0, 0.0,  size.width, size.height), layerPaint);
+    layerPaint.blendMode = BlendMode.dstIn;
+    canvas.saveLayer(
+        new Rect.fromLTRB(0.0, 0.0, size.width, size.height), layerPaint);
 
-    canvas.drawCircle(new Offset(halfSizeWidth, halfSizeHeight), radius-circleStrokeWidth, _paints);
+    canvas.drawCircle(new Offset(halfSizeWidth, halfSizeHeight),
+        radius - circleStrokeWidth, _paints);
     canvas.restore();
     canvas.restore();
 
-
-    _paints.style=PaintingStyle.stroke;
-    _paints.color=strokeCircleColor;
-    _paints.strokeWidth=circleStrokeWidth;
-    canvas.drawCircle(new Offset(halfSizeWidth, halfSizeHeight), radius, _paints);
+    _paints.style = PaintingStyle.stroke;
+    _paints.color = strokeCircleColor;
+    _paints.strokeWidth = circleStrokeWidth;
+    canvas.drawCircle(
+        new Offset(halfSizeWidth, halfSizeHeight), radius, _paints);
 
     TextPainter textPainter = new TextPainter();
     textPainter.textDirection = TextDirection.ltr;
-    textPainter.text = new TextSpan(text: (percentage*100).toInt().toString()+"%",
-      style: textStyle,);
+    textPainter.text = new TextSpan(
+      text: (percentage * 100).toInt().toString() + "%",
+      style: textStyle,
+    );
     textPainter.layout();
-    double textStarPositionX = halfSizeWidth-textPainter.size.width/2;
-    double textStarPositionY = halfSizeHeight-textPainter.size.height/2;
+    double textStarPositionX = halfSizeWidth - textPainter.size.width / 2;
+    double textStarPositionY = halfSizeHeight - textPainter.size.height / 2;
     textPainter.paint(canvas, new Offset(textStarPositionX, textStarPositionY));
   }
 
@@ -294,11 +295,9 @@ class BezierCurvePainter extends CustomPainter{
 }
 
 class WaterController {
-  WaveProgressBarState bezierCurveState;
+  WaveProgressBarState? bezierCurveState;
 
-  void  changeWaterHeight(double h){
-    if(bezierCurveState!=null) {
-      bezierCurveState.changeWaterHeight(h);
-    }
+  void changeWaterHeight(double h) {
+    bezierCurveState?.changeWaterHeight(h);
   }
 }
